9e89fa381fc61125d0d4bbd8203fe26d400aec42
pawinhon1 <33918211+pawinhon1@users.noreply.github.com>
2020-11-13 14:32:01 +0700

    Add files via upload

---
+++ 01_php_form_handling.php
@@ -0,0 +1,9 @@
+﻿<html>
+ <haed></head>
+ <body>
+ <form name="frm1" method="post" action="">
+ ชื่อ : <input type="text" name="txt_name"><br>
+ <input type="submit" name="summit_form" value="ส่ง">
+ </form>
+ </body>
+</html>
